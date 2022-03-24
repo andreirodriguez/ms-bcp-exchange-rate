a687f6749f837f1678dd63b4b0c4113cfc26ec51
andreirodriguez <arodriguez.impress@gmail.com>
2022-03-23 19:08:16 -0500

    #32 Algoritmo numeros primos
    #32 Algoritmo numeros primos

--- src/main/java/pe/bcp/exchangerate/api/controller/CurrencyController.java
+++ src/main/java/pe/bcp/exchangerate/api/controller/CurrencyController.java
@@ -1,12 +1,17 @@
 package pe.bcp.exchangerate.api.controller;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,6 +41,26 @@ public class CurrencyController {
                 .subscribeOn(Schedulers.io())
                 .map(list -> ResponseEntity.ok(CurrencyMapper.ToArrayCurrencyResponse(list)));
 	}	
-	
+
+	@PostMapping(value = "/primes",produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<Integer>> getPrimes(@RequestBody List<Integer> numbers) {
+		List<Integer> numbersPrimes = new ArrayList<Integer>();
+		
+		int count;
+		for(Integer number:numbers)
+		{
+			count=0;
+			for(int i=1;i<=number;i++)
+				if(number%i==0)
+					count++;
+			
+			if(count==2) numbersPrimes.add(number);
+		}			
+		
+		Collections.sort(numbersPrimes);
+		
+		return new ResponseEntity<List<Integer>>(numbersPrimes,HttpStatus.OK);
+				
+	}		
 	
 }
